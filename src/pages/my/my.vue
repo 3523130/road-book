76b09f43eed690a5bb6548d3fdcd5a7fb5cf7f54
3523130 <3523130@qq.com>
2024-10-08 17:11:53 +0800

    init

---
+++ src/pages/my/my.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="my">
+    <view class="person">
+      <image src="" />
+      <view class="info">
+        <view class="name">风</view>
+        <view class="region">深圳</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
