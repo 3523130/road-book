89e61c8d9f4d97f1516833bace9047231bccc7e2
3523130 <3523130@qq.com>
2024-10-08 18:31:20 +0800

    refactor: tabBar

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,12 +1,16 @@
 <template>
   <view class="my">
-    <view class="person">
-      <image src="" />
+    <div class="head">
+      <view class="person">
+      <div class="img">
+        <img src="/static/head-img.jpg" alt="">
+      </div>
       <view class="info">
         <view class="name">风</view>
         <view class="region">深圳</view>
       </view>
     </view>
+    </div>
   </view>
 </template>
 
@@ -14,4 +18,25 @@
 import { ref } from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.head{
+  .person{
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    .img{
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      overflow: hidden;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .info{
+      margin-left: 10px;
+    }
+  }
+}
+</style>
